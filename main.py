a989fb2fa0a04d91b539577e0d2089211e79cf6f
kisita <kisita2002@yahoo.fr>
2018-12-19 05:43:56 -0800

    Remove everything related to firebase in the main application

--- main.py
+++ main.py
@@ -3,24 +3,13 @@ Created on Dec 18, 2018
 
 @author: Hugues Kisita
 '''
-import firebase_admin
 
-from firebase_admin import credentials
 from parser import Parser
 from statistique import Statistique
 
 
-
-    
-def initApp():
-    # Fetch the service account key JSON file contents
-    cred = credentials.Certificate('/home/firebase/firebase/caritas-50fab-b10622860737.json')
-    # Initialize the application with a service account, granting admin privileges
-    firebase_admin.initialize_app(cred, {'databaseURL': 'https://caritas-50fab.firebaseio.com/'})
-
 if __name__ == '__main__':
     # Must be called before calling any function
-    initApp()
     parser = Parser();
     
     stat = Statistique(parser.getQuestions());
