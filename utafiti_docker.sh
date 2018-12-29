8ecd12acaec133d9d95d32c3a5caba41d4292295
kisita <kisita2002@yahoo.fr>
2018-12-29 08:14:46 +0100

    Kubernetes and docker commands for utafiti deployment

--- utafiti_docker.sh
+++ utafiti_docker.sh
@@ -1,2 +1,14 @@
 docker build -t gcr.io/${PROJECT_ID}/utafiti-app:v1 .
-docker push  gcr.io/${PROJECT_ID}/utafiti-app:v1 .
+docker push  gcr.io/${PROJECT_ID}/utafiti-app:v1 
+
+#Create container cluster before using kubectl
+gcloud container clusters create utafiti-cluster --num-nodes=1 --zone=europe-west1
+
+
+#create utafiti pod containing two containers. nginx and utafiti processing
+kubectl create -f utafiti-deployment.yaml
+
+#Expose my application to internet
+kubectl expose deployment utafiti-deployment --type=LoadBalancer --port 80 --target-port 80
+
+
